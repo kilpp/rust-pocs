ab01e6b975c85c5fadaf93729bd5ac9e71f6cc31
Guilherme Kilpp <guikilpp@gmail.com>
2026-03-06 23:46:46 -0300

    Added hittable

---
+++ rust-raytracer/src/hittable.rs
@@ -0,0 +1,58 @@
+use crate::material::Material;
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+pub struct HitRecord<'a> {
+    pub point: Vec3,
+    pub normal: Vec3,
+    pub t: f64,
+    pub front_face: bool,
+    pub material: &'a dyn Material,
+}
+
+impl<'a> HitRecord<'a> {
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: Vec3) {
+        self.front_face = ray.direction.dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
+}
+
+pub trait Hittable: Send + Sync {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord<'_>>;
+}
+
+pub struct HittableList {
+    pub objects: Vec<Box<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        Self {
+            objects: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, object: Box<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord<'_>> {
+        let mut closest = t_max;
+        let mut hit_record = None;
+
+        for object in &self.objects {
+            if let Some(rec) = object.hit(ray, t_min, closest) {
+                closest = rec.t;
+                hit_record = Some(rec);
+            }
+        }
+
+        hit_record
+    }
+}

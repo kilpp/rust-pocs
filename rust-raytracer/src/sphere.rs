18b968451212012a6b1432fbdeca34d4a87f6931
Guilherme Kilpp <guikilpp@gmail.com>
2026-03-06 23:46:08 -0300

    Added sphere

---
+++ rust-raytracer/src/sphere.rs
@@ -0,0 +1,59 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::material::Material;
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+pub struct Sphere {
+    pub center: Vec3,
+    pub radius: f64,
+    pub material: Box<dyn Material>,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3, radius: f64, material: Box<dyn Material>) -> Self {
+        Self {
+            center,
+            radius,
+            material,
+        }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord<'_>> {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.length_squared();
+        let half_b = oc.dot(ray.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+
+        // Find nearest root in acceptable range
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || root > t_max {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || root > t_max {
+                return None;
+            }
+        }
+
+        let point = ray.at(root);
+        let outward_normal = (point - self.center) / self.radius;
+
+        let mut rec = HitRecord {
+            point,
+            normal: outward_normal,
+            t: root,
+            front_face: true,
+            material: self.material.as_ref(),
+        };
+        rec.set_face_normal(ray, outward_normal);
+
+        Some(rec)
+    }
+}
